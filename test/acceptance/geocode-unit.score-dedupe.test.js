07310f129454dde691d8aedd662952872f8af44d
Andrew Pendleton <andrew@mapbox.com>
2021-01-27 23:40:07 -0500

    move where the ghost feature dedup happens in verifymatch, and add tests

--- test/acceptance/geocode-unit.score-dedupe.test.js
+++ test/acceptance/geocode-unit.score-dedupe.test.js
@@ -8,6 +8,7 @@ const queue = require('d3-queue').queue;
 const { queueFeature, buildQueued } = require('../../lib/indexer/addfeature');
 
 const conf = {
+    region: new mem({ maxzoom: 6 }, () => {}),
     place: new mem({ maxzoom: 6 }, () => {})
 };
 const c = new Carmen(conf);
@@ -35,28 +36,75 @@ tape('index data', (t) => {
             id: 2,
             properties: {
                 'carmen:text': 'fake place 1',
-                'carmen:center': [0,1],
+                'carmen:center': [0,1.01],
                 'carmen:score': 1
             },
             geometry: {
                 type: 'Point',
-                coordinates: [0,1]
+                coordinates: [0,1.01]
+            }
+        },
+        cb
+    ));
+    q.defer((cb) => queueFeature(
+        conf.place,
+        {
+            id: 3,
+            properties: {
+                'carmen:text': 'fake place',
+                'carmen:center': [0,0],
+                'carmen:score': 1
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [0,0]
+            }
+        },
+        cb
+    ));
+    q.defer((cb) => queueFeature(
+        conf.region,
+        {
+            id: 10,
+            properties: {
+                'carmen:text': 'region',
+                'carmen:center': [0,0],
+                'carmen:score': 1
+            },
+            geometry: {
+                type: 'Polygon',
+                coordinates: [[[-1,-1],[1,-1],[1,1],[-1,1],[-1,-1]]]
             }
         },
         cb
     ));
 
-    q.defer((cb) => buildQueued(conf.place, cb));
+    q.defer((cb) => buildQueued(conf.place, () => buildQueued(conf.region, cb)));
 
     q.awaitAll(t.end);
 });
 
 tape('test deduping features with identical text preserving the feature with a higher score', (t) => {
     c.geocode('fake place 1', { limit_verify: 5 }, (err, res) => {
-        t.equals(res.features.length, 1, 'returned the feature with higher score');
-        t.equals(res.features[0].id, 'place.2', 'returned fake place 1 with id = place.2');
+        t.error(err);
+        t.equals(res.features.length, 2, 'returned two of the three features');
+        t.equals(res.features[0].id, 'place.2', 'returned fake place 1 with id = place.2 (the higher-scored one)');
         t.equals(res.features[0].place_name, 'fake place 1', 'returned fake place 1 feature with a higher score');
+        t.equals(res.features.filter((f) => f.id === 'place.1').length, 0, 'place.1 was deduped away and not returned');
         t.ifError(err);
+        t.end();
+    });
+});
+
+tape('test deduping features with identical text preserving the feature with a higher score', (t) => {
+    c.geocode('fake place 1 region', { limit_verify: 5 }, (err, res) => {
+        t.error(err);
+        t.equals(
+            res.features[0].id,
+            'place.1',
+            'ghost feature is not deduped away because it spatially aligns and non-ghost feature does not'
+        );
+        t.equals(res.features[0].relevance, 1, 'winning feature has full relevance');
         t.end();
     });
 });
